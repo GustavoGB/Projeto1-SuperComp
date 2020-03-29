90c39e5b098834732e977b9ad8835894cc5426a8
GustavoGB <gustavo.gobetti98@gmail.com>
2020-03-29 14:36:33 -0700

    Passando os tipos de variaveis e a func

---
+++ build/CMakeFiles/ingenuo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ingenuo.dir/ingenuo.cpp.o"
+  "ingenuo.pdb"
+  "ingenuo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ingenuo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ingenuo.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+
+
+/* Vamos traduzir o codigo python antigo para C++, para tal vamos replicar a funcao escolhe_aluno
+Como recordacao, temos que a escolhe_aluno recebe os seguintes argumentos: 
+    * Prefs (preferencia dos alunos)
+    * Aluno_projeto()
+    * Vagas
+    * satisfacao_atual 
+    * Melhor 
+    * i = 0
+*/
+
+/* Quais tipos de dados serao cada um destes argumentos ?
+    * Prefs - > vector de int
+    * Aluno projeto -> vector de int
+    * Vagas - > vector de int 
+    * i -> int = 0
+    * satisfacao_atual -> int = 0
+    * Melhor ???? tupla ? 
+*/
+
+void escolhe_aluno(){
+
+}
+
+
+int main() {
+
+    std::cout << "Hello World!\n";
+    return 0;
+
+
+
+}
