0d9b8d3559cbf7d2e1796736a30589d165a16f3a
GustavoGB <gustavo.gobetti98@gmail.com>
2020-03-29 19:05:28 -0700

    Cmake tudo ok, primeira func quase pronta

--- ingenuo.cpp
+++ ingenuo.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <tuple>
 
 using namespace std;
-
 
 /* Vamos traduzir o codigo python antigo para C++, para tal vamos replicar a funcao escolhe_aluno
 Como recordacao, temos que a escolhe_aluno recebe os seguintes argumentos: 
@@ -21,19 +21,57 @@ Como recordacao, temos que a escolhe_aluno recebe os seguintes argumentos:
     * Vagas - > vector de int 
     * i -> int = 0
     * satisfacao_atual -> int = 0
-    * Melhor ???? tupla ? 
+    * Melhor tupla(vector int, int)  
+    * Uma flag caso o melhor caso ja tenha sido encontrado 
 */
 
-void escolhe_aluno(){
+/* Quais condicoes e loops temos dentro desta funcao? 
+O que eles estao fazendo? Como estao alterando estas estruturas
+de dados? 
 
+    * if i == len(aluno_projeto) 
+    --> igualar o i para pegar o tamanho dos projetos
+    disponiveis para os alunos. C++ podemos usar o .size ja que 
+    queremos sabe o tamanho do Vetor!
+    i == aluno_projeto.size() 
+    * std::cout << "(" << std::get<0>(t) << ", " << std::get<1>(t) -> (Exemplo tupla)
+*/
+
+void escolhe_alunos(vector<vector<int>> prefs,
+                    vector<int> aluno_projeto,
+                    vector<int> vagas,
+                    tuple<vector<int>, int> &melhor,
+                    int i = 0, int satisfacao_atual = 0,
+                    bool isNull = false)
+{
+    if (i == aluno_projeto.size())
+    {
+        if (isNull == true)
+        {
+            get<0>(melhor) = aluno_projeto;
+            get<1>(melhor) = satisfacao_atual;
+            isNull = true;
+        }
+        if (satisfacao_atual > get<1>(melhor))
+        {
+            get<0>(melhor) = aluno_projeto;
+            get<1>(melhor) = satisfacao_atual;
+            isNull = true;
+        }
+    }
+
+    for (int proj_atual = 0; proj_atual < prefs[1].size(); proj_atual++)
+    {
+        if (vagas[proj_atual] > 0)
+    }
 }
 
+int main()
+{
 
-int main() {
+    //Receber o input gerado pelo python
 
-    std::cout << "Hello World!\n";
+    std::cout << "Ola eu sou o bebeto"
+              << "\n";
     return 0;
-
-
-
 }
