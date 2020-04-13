59713a7d302d215e8aebd12087a63d6ffe4654b1
GustavoGB <gustavo.gobetti98@gmail.com>
2020-04-13 20:43:02 -0300

    coloquei o if do branch and bond falta busca local- muito confuso

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -49,5 +49,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/ingenuo.dir/DependInfo.cmake"
+  "CMakeFiles/projeto1Fim.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/projeto1Fim.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projeto1Fim.dir/ingenuo.cpp.o"
+  "projeto1Fim"
+  "projeto1Fim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projeto1Fim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
